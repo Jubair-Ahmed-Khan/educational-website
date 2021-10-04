a7fbfa2df72f8db2e42533163823cb6ed17e8606
Jubair-Ahmed-Khan <jubair.mbstu@gmail.com>
2021-10-04 15:47:11 +0600

    404 edited

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import './NotFound.css';
 
 const NotFound = () => {
     return (
 
         // 404 error message container 
-        <div className="error-message">
-
+        <div className="container">
+            <div className="w-100 mx-auto">
+                <img className="img-fluid" src="./images/error.png" alt="404_error" />
+            </div>
         </div>
+
+
+
     );
 };
 
